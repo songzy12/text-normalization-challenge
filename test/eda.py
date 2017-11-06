c5a3fe6271809f8e03da786ca89d98b56ebab91e
songzy <songzy.thu@qq.com>
2017-11-06 20:42:35 +0800

    more baseline

---
+++ test/eda.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv('input/en_train.csv')
+
+print df.head(10)
+print df['class'],unique()
+
+length = df.groupby(['sentence_id'], as_index=False).count()
+length = length.groupby(['before'], as_index=False).count()
+length['before'].describe()
+
+df[df['class']=='PUNCT'].head()
+df[df['class']=='DATE'].head(10)
